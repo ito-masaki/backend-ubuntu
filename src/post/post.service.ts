911968a81774aabcf62b41fd7c69fec34293bf22
masaki <03260812masaki@gmail.com>
2025-04-08 10:41:16 +0900

    ss

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -15,22 +15,29 @@ export class PostService {
 
   async createpost(message: string, token: string) {
     const now = new Date();
+    console.log(`Postされた時刻${now} ,${message}, ${token}`); //デバック
     const auth = await this.authRepository.findOne({
       where: {
         token: Equal(token),
         expire_at: MoreThan(now),
       },
     });
+    // 認証されていなければエラーを返す。
     if (!auth) {
       throw new ForbiddenException();
     }
+
+    // 短文投稿DBへdataを格納
     const record = {
       user_id: auth.user_id,
       content: message,
     };
     await this.microPostRepository.save(record);
+    console.log(`登録されたPost${record}`);
+    return record;
   }
 
+  //投稿を取得する関数, 存在しない場合の初期値を置いている！！
   async getList(token: string, start: number = 0, nr_records: number = 1) {
     const now = new Date();
     const auth = await this.authRepository.findOne({
@@ -42,6 +49,7 @@ export class PostService {
     if (!auth) {
       throw new ForbiddenException();
     }
+
     const qb = await this.microPostRepository
       .createQueryBuilder('micro_post')
       .leftJoinAndSelect('user', 'user', 'user.id=micro_post.user_id')
@@ -54,12 +62,15 @@ export class PostService {
       .orderBy('micro_post.created_at', 'DESC')
       .offset(start)
       .limit(nr_records);
+
+    // 送信するための型を指定
     type ResultType = {
       id: number;
       content: string;
       user_name: string;
       created_at: Date;
     };
+
     const records = await qb.getRawMany<ResultType>();
     console.log(records);
     return records;
