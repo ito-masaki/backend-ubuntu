6f0db8c15b1d1f349614b2aa02a5750b2ffd2c2a
masaki <03260812masaki@gmail.com>
2025-07-24 14:22:53 +0900

    0724

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,22 +5,30 @@ import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // アプリケーション全体で利用可能にする
+    }),
     UserModule,
     PostModule,
-    AuthModule, // NestJSがPostgreSQLと接続、勝手に読み込む
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'myuser',
-      password: 'mypassword',
-      database: 'my-db',
-      autoLoadEntities: true,
-      synchronize: false, // 自動同期を無効化
-      // entities: ['./src/entities/*.ts'],
+    AuthModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], // DIするためのおまじない
+      inject: [ConfigService], // ConfigServiceを注入
+      // useFactoryで.envの値を元に接続情報を生成
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'), // .envのNAMEキーを指定
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
     }),
   ],
   controllers: [AppController],
